fd29a606ef94f47829a3f7fc6c676cdb4e829cad
oloffnsk <139168904+oloffnsk@users.noreply.github.com>
2024-12-10 01:47:35 -0500

    removed theme files and cleaned up

---
+++ README.md
@@ -0,0 +1,6 @@
+\/ Theme, Icon Pack, And Cursor \/
+--- https://github.com/vinceliuice/Fluent-gtk-theme
+
+--- https://www.pling.com/p/2042592
+
+--- https://www.pling.com/s/Gnome/p/1309810/
--- install.sh
+++ install.sh
@@ -5,31 +5,19 @@ echo "Make sure you have a backup of your config files.
 This will overwrite similar files. This script needs
 sudo permissions."
 echo
-echo "fcitx, fcitx-mozc, and their required dependencies
-will not be installed, but you will be prompted to istall
-rofi, picom, nautilus, nautilus-open-any-terminal, noto CJK fonts, and mpv..."
+echo "You will be prompted to istall
+rofi, picom, noto CJK fonts, and mpv..."
 echo
-read -p "Continue? " -n  1 -r
+read -p "Continue? y/n" -n  1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-	if ! command -v yay 2>&1 >/dev/null
-	then
-		echo "Please install the yay AUR helper"
-		exit 1
-	fi
-
-	yay -S nautilus-open-any-terminal
-	sudo pacman -S picom rofi nautilus mpv noto-fonts-cjk
+	sudo pacman -S picom rofi mpv noto-fonts-cjk
 	cp -r -f .config ~/
-	cp -r -f .themes ~/
 	sleep 1
 	echo
 	sudo mv -f ~/.config/rofi/rofiblurblack.rasi /usr/share/rofi/themes/
-	sudo mv -f ~/.themes/Os-Catalina-Night /usr/share/icons
-	sudo mv -f ~/.themes/MacOS-Pixel-vr4-Linux/MacOS-Pixel-vr4 /usr/share/icons
-
 	echo "[Desktop Entry]
 Encoding=UTF-8
 Version=0.9.4
@@ -56,6 +44,9 @@ file in the mozc .config folder."
 	echo
 	echo "To open rofi, it is super+r.
 To open xterm, it is super+e"
+	echo
+	echo "The theme, icon pack, and cursor I use are recommended to be used in tandem with my config files.
+ You can find the link to these in the README."
 	echo
 
 fi
